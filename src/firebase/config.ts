b4f3550b53829e3caf91a97fea3b770a8098c0f0
Manas Porwal <manasporwal3@gmail.com>
2026-03-28 01:39:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3079132686-dc38a",
+  "appId": "1:821596194386:web:617139d49aac0ab25c818b",
+  "apiKey": "AIzaSyCyibK3XCOZ605qzczrJN5iZNlHxNGjYoc",
+  "authDomain": "studio-3079132686-dc38a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "821596194386"
+};
